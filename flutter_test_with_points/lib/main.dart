db61c30fbd0955ecac3f895fb8b2fb45f2fa8351
Dmitriy <Dimamakarov1941@yandex.ru>
2021-05-07 16:22:27 +0300

    rename test project

---
+++ flutter_test_with_points/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_test_with_points/Test/testStructure.dart';
+import 'package:flutter_test_with_points/Test/testWidget.dart';
+import 'package:flutter_test_with_points/result/resultWidget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _questionIndex = 0;
+  final List<TestStructure> _questions = [
+    TestStructure("Кто я?", ["Дима", "Дима", "Дима", "Дмитрий"]),
+    TestStructure("Кто ты?", ["Алексей", "Дима", "Дима", "Дмитрий"])
+  ];
+  void _nextQuestion() {
+    setState(() {
+      ++_questionIndex;
+    });
+  }
+
+  void _retry() {
+    setState(() {
+      _questionIndex = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+            child: _questionIndex < _questions.length
+                ? Test(_questions[_questionIndex], _nextQuestion)
+                : Result(_retry)));
+  }
+}
---
+++ readme.md
@@ -0,0 +1,32 @@
+<p align="center">
+  <a href="" rel="noopener">
+ <img width=200px height=200px src="assets\images\flutter logo.png" alt="Project logo"></a>
+</p>
+
+<h3 align="center">Flutter education projects</h3>
+
+---
+
+<p align="center">
+ My training in the flutter framework
+</p>
+
+## 🧭Navigation
+
+* [About](#about)
+* [Projects](#List_of_projects)
+* [Author](#author)
+
+## 🧐 About <a name = "about"></a>
+
+Here you can find educational projects on flutter that I created during the courses.
+
+## 📝Projects <a name = "List_of_projects"></a>
+
+1. Test with points
+
+##
+
+## ✍️ Authors <a name = "authors"></a>
+
+* [@mentoster](https://github.com/mentoster) -  work
