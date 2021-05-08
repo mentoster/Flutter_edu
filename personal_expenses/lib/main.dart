39e7cc71a1566c2a2b946ab7e986ca3b31af1e2c
Dmitriy <Dimamakarov1941@yandex.ru>
2021-05-08 12:19:02 +0300

    add title list

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter personal expenses'),
     );
   }
 }
@@ -20,13 +21,42 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-
+  final List<Transaction> transactions = [
+    Transaction(
+      't2',
+      'Weekly Griceries',
+      17.53,
+      DateTime.now(),
+    ),
+    Transaction(
+      't1',
+      'New Shoes',
+      69.99,
+      DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Flutter app'),
+          title: Container(child: Text('Flutter app')),
+          elevation: 5,
         ),
-        body: Center(child: Text("Hello world!")));
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+              child: Text('Chart'),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Text(tx.title),
+                );
+              }).toList(),
+            ),
+          ],
+        ));
   }
 }
